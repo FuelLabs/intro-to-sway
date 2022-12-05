45f09a0a706dd07c6d33603f9ad74eba7a8cd386
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-12-05 16:23:26 -0700

    add owner and commission

--- src/main.sw
+++ src/main.sw
@@ -7,8 +7,8 @@ use std::{
     },
     constants::BASE_ASSET_ID,
     auth::{AuthError, msg_sender},
-    call_frames::msg_asset_id,
-    context::{ msg_amount },
+    call_frames::{msg_asset_id, contract_id},
+    context::{ msg_amount, this_balance },
     token::transfer,
     identity::Identity
 };
@@ -22,7 +22,8 @@ struct Item {
 
 enum InvalidError {
     IncorrectAssetId: (),
-    NotEnoughTokens: ()
+    NotEnoughTokens: (),
+    OnlyOwner: ()
 }
 
 abi SwayStore {
@@ -31,52 +32,114 @@ abi SwayStore {
 
     #[storage(read, write)]
     fn buy_item(item_id: u64);
+
+    #[storage(read)]
+    fn get_item(item_id: u64) -> Item;
+
+    #[storage(read, write)]
+    fn initialize_owner() -> Identity;
+
+    #[storage(read)]
+    fn withdraw_funds();
 }
 
 storage {
     // counter for total items listed
     item_counter: u64 = 0,
-
     // map of item IDs to Items
     item_map: StorageMap<u64, Item> = StorageMap{},
-
     // a vector of all purchases
     // stores the item ID and buyer identity for all purchases
     purchases: StorageVec<(u64, Identity)> = StorageVec{},
-
+    owner: Option<Identity> = Option::None
 }
 
 
 impl SwayStore for Contract {
     #[storage(read, write)]
     fn list_item(price: u64, metadata: str[20]){
+        // increment the item counter
         storage.item_counter = storage.item_counter + 1;
+        //  get the message sender
         let sender: Result<Identity, AuthError> = msg_sender();
+        // configure the item
         let new_item: Item = Item {
             id: storage.item_counter,
             price: price,
             owner: sender.unwrap(),
             metadata: metadata
         };
-
+        // save the new item to storage using the counter value
         storage.item_map.insert(storage.item_counter, new_item);
     }
 
     #[storage(read, write)]
     fn buy_item(item_id: u64){
+        // get the asset id for the asset sent
         let asset_id = msg_asset_id();
-        let amount = msg_amount();
-        let item = storage.item_map.get(item_id);
-
-        // require payment
+        // require that the correct asset was sent
         require(asset_id == BASE_ASSET_ID, InvalidError::IncorrectAssetId);
+
+        // get the amount of coins sent
+        let amount = msg_amount();
+        // get the item to buy
+        let item = storage.item_map.get(item_id);
+        // require that the amount is at least the price of the item
         require(amount >= item.price, InvalidError::NotEnoughTokens);
 
+        // get the identity of the sender
         let sender: Result<Identity, AuthError> = msg_sender();
 
+        // add the purchase to the storage vector
         storage.purchases.push((item_id, sender.unwrap()));
+        // only charge commission if price is more than 10
+        if amount > 100 {
+            // for every 100 coins, the contract keeps 5
+            let comission = amount * 10 / 200;
+            let new_amount = amount - comission;
+            // send the payout minus commission to the seller
+            transfer(new_amount, asset_id, item.owner);
+        } else {
+            // send the full payout to the seller
+            transfer(amount, asset_id, item.owner);
+        }
+        
+    }
 
-          // send the payout
-        transfer(amount, asset_id, item.owner);
+    #[storage(read)]
+    fn get_item(item_id: u64) -> Item{
+        // returns the item for the given item_id
+        storage.item_map.get(item_id)
+    }
+
+    #[storage(read, write)]
+    fn initialize_owner() -> Identity{
+        let owner = storage.owner;
+        // make sure the owner has NOT been initialized
+        require(owner.is_none(), InvalidError::OnlyOwner);
+        // get the identity of the sender
+        let sender: Result<Identity, AuthError> = msg_sender(); 
+        // set the owner to the sender's identity
+        storage.owner = Option::Some(sender.unwrap());
+        // return the owner
+        sender.unwrap()
+    }
+
+    #[storage(read)]
+    fn withdraw_funds(){
+        let owner = storage.owner;
+        // make sure the owner has been initialized
+        require(owner.is_none() == false, InvalidError::OnlyOwner);
+
+        let sender: Result<Identity, AuthError> = msg_sender(); 
+        // require the sender to be the owner
+        require(sender.unwrap() == owner.unwrap(), InvalidError::OnlyOwner);
+
+        // get the current balance of this contract for the base asset
+        let amount = this_balance(BASE_ASSET_ID);
+        // send the amount to the owner
+        if amount > 0 {
+            transfer(amount, BASE_ASSET_ID, owner.unwrap());
+        }
     }
 }
--- tests/harness.rs
+++ tests/harness.rs
@@ -7,7 +7,7 @@ async fn get_contract_instance() -> (MyContract, ContractId, Vec<WalletUnlocked>
     // Launch a local network and deploy the contract
     let wallets = launch_custom_provider_and_get_wallets(
         WalletsConfig::new(
-            Some(2),             /* Two wallets */
+            Some(3),             /* Two wallets */
             Some(1),             /* Single coin (UTXO) */
             Some(1_000_000_000), /* Amount per coin */
         ),
@@ -33,6 +33,58 @@ async fn get_contract_instance() -> (MyContract, ContractId, Vec<WalletUnlocked>
 
     (instance, id.into(), wallets)
 }
+
+#[tokio::test]
+async fn can_set_owner() {
+    let (instance, _id, wallets) = get_contract_instance().await;
+
+    // get access to a test wallet
+    let wallet_1 = wallets.get(0).unwrap();
+
+    // initialize wallet_1 as the owner
+    let owner_result = instance
+        .with_wallet(wallet_1.clone())
+        .unwrap()
+        .methods()
+        .initialize_owner()
+        .call()
+        .await
+        .unwrap();
+
+    // make sure the returned identity matches wallet_1
+    assert!(Identity::Address(wallet_1.address().into()) == owner_result.value);
+}
+
+// #[tokio::test]
+// #[should_panic]
+// async fn can_set_owner_only_once() {
+//     let (instance, _id, wallets) = get_contract_instance().await;
+
+//     // get access to some test wallets
+//     let wallet_1 = wallets.get(0).unwrap();
+//     let wallet_2 = wallets.get(1).unwrap();
+
+//     // initialize wallet_1 as the owner
+//     let _owner_result = instance
+//         .with_wallet(wallet_1.clone())
+//         .unwrap()
+//         .methods()
+//         .initialize_owner()
+//         .call()
+//         .await
+//         .unwrap();
+
+//     // this should fail
+//     // try to set the owner from wallet_2
+//     let _fail_owner_result = instance
+//         .with_wallet(wallet_2.clone())
+//         .unwrap()
+//         .methods()
+//         .initialize_owner()
+//         .call()
+//         .await
+//         .unwrap();
+// }
 
 #[tokio::test]
 async fn can_list_and_buy_item() {
@@ -65,7 +117,6 @@ async fn can_list_and_buy_item() {
     // call params to send the project price in the buy_item fn
     let call_params = CallParameters::new(Some(item_1_price), Some(BASE_ASSET_ID), None);
 
-
     // buy item 1 from wallet_2
     let _item_1_purchase = instance
         .with_wallet(wallet_2.clone())
@@ -85,4 +136,90 @@ async fn can_list_and_buy_item() {
     // make sure the price was transferred from wallet_2 to wallet_1
     assert!(balance_1 == 1000000015);
     assert!(balance_2 == 999999985);
+
+    let item_1 = instance.methods().get_item(1).call().await.unwrap();
+
+    assert!(item_1.value.price == item_1_price);
+    assert!(item_1.value.id == 1);
+}
+
+#[tokio::test]
+async fn can_withdraw_funds() {
+    let (instance, _id, wallets) = get_contract_instance().await;
+    // Now you have an instance of your contract you can use to test each function
+
+    // get access to some test wallets
+    let wallet_1 = wallets.get(0).unwrap();
+    let wallet_2 = wallets.get(1).unwrap();
+    let wallet_3 = wallets.get(2).unwrap();
+
+    // initialize wallet_1 as the owner
+    let owner_result = instance
+        .with_wallet(wallet_1.clone())
+        .unwrap()
+        .methods()
+        .initialize_owner()
+        .call()
+        .await
+        .unwrap();
+
+    // make sure the returned identity matches wallet_1
+    assert!(Identity::Address(wallet_1.address().into()) == owner_result.value);
+
+    // item 1 params
+    let item_1_metadata: fuels::core::types::SizedAsciiString<20> = "metadata__url__here_"
+        .try_into()
+        .expect("Should have succeeded");
+    let item_1_price: u64 = 15_000;
+
+    // list item 1 from wallet_2
+    let _item_1_result = instance
+        .with_wallet(wallet_2.clone())
+        .unwrap()
+        .methods()
+        .list_item(item_1_price, item_1_metadata)
+        .call()
+        .await
+        .unwrap();
+
+    // Bytes representation of the asset ID of the "base" asset used for gas fees.
+    const BASE_ASSET_ID: AssetId = AssetId::new([0u8; 32]);
+
+    // call params to send the project price in the buy_item fn
+    let call_params = CallParameters::new(Some(item_1_price), Some(BASE_ASSET_ID), None);
+
+    // buy item 1 from wallet_3
+    let _item_1_purchase = instance
+        .with_wallet(wallet_3.clone())
+        .unwrap()
+        .methods()
+        .buy_item(1)
+        .append_variable_outputs(1)
+        .call_params(call_params)
+        .call()
+        .await
+        .unwrap();
+
+    // withdraw the balance from the owner's wallet
+    let _resp = instance
+        .with_wallet(wallet_1.clone())
+        .unwrap()
+        .methods()
+        .withdraw_funds()
+        .append_variable_outputs(1)
+        .call()
+        .await
+        .unwrap();
+
+    // check the balances of wallet_1 and wallet_2
+    let balance_1: u64 = wallet_1.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+    let balance_2: u64 = wallet_2.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+    let balance_3: u64 = wallet_3.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+
+    // println!("BALANCE 1: {:?}", balance_1);
+    assert!(balance_1 == 1000000750);
+    // println!("BALANCE 2: {:?}", balance_2);
+    assert!(balance_2 == 1000014250);
+    // println!("BALANCE 3: {:?}", balance_3);
+    assert!(balance_3 == 999985000);
 }
