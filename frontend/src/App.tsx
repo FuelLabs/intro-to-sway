8c1c98ff2889aa95d2fdbfbaaf15a1739b37faf9
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-09-27 17:11:59 -0600

    use wallet react hooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect, useMemo } from "react";
-import { Fuel } from '@fuel-wallet/sdk';
-import { WalletLocked } from "fuels";
+import { useState, useMemo } from "react";
+import { useFuel, useIsConnected, useAccount, useWallet } from '@fuel-wallet/react';
 import { ContractAbi__factory } from "./contracts"
 import AllItems from "./components/AllItems";
 import ListItem from "./components/ListItem";
@@ -9,30 +8,19 @@ import "./App.css";
 const CONTRACT_ID = "0xe924cde59c8b07fe4155f484038cdab8a027e3549eda80022c7c515a4933a594"
 
 function App() {
-  const [wallet, setWallet] = useState<WalletLocked>();
   const [active, setActive] = useState<'all-items' | 'list-item'>('all-items');
-  const [isConnected, setIsConnected] = useState(false);
-  const fuel = useMemo(() => new Fuel(), []);
-
-  useEffect(() => {
-    async function getAccounts() {
-      const currentAccount = await fuel.currentAccount();
-      const tempWallet = await fuel.getWallet(currentAccount)
-      setWallet(tempWallet)
-
-      const connected = await fuel.isConnected()
-      setIsConnected(connected);
-    }
-    if (fuel) getAccounts();
-  }, [fuel]);
+  const fuelObj = useFuel();
+  const isConnectedObj = useIsConnected();
+  const accountObj = useAccount();
+  const walletObj = useWallet({ address: accountObj.account });
 
   const contract = useMemo(() => {
-    if (fuel && wallet) {
-      const contract = ContractAbi__factory.connect(CONTRACT_ID, wallet);
+    if (walletObj.wallet) {
+      const contract = ContractAbi__factory.connect(CONTRACT_ID, walletObj.wallet);
       return contract;
     }
     return null;
-  }, [fuel, wallet]);
+  }, [walletObj]);
 
   return (
     <div className="App">
@@ -41,21 +29,33 @@ function App() {
       </header>
       <nav>
         <ul>
-          <li className={active === 'all-items' ? "active-tab" : ""} onClick={() => setActive('all-items')}>See All Items</li>
-          <li className={active === 'list-item' ? "active-tab" : ""} onClick={() => setActive('list-item')}>List an Item</li>
+          <li 
+          className={active === 'all-items' ? "active-tab" : ""} 
+          onClick={() => setActive('all-items')}
+          >
+            See All Items
+          </li>
+          <li 
+          className={active === 'list-item' ? "active-tab" : ""} 
+          onClick={() => setActive('list-item')}
+          >
+            List an Item
+          </li>
         </ul>
       </nav>
 
-      {fuel ? (
+      {fuelObj.fuel ? (
         <div>
-          { isConnected ? (
+          { isConnectedObj.isConnected ? (
             <div>
               {active === 'all-items' && <AllItems contract={contract} />}
               {active === 'list-item' && <ListItem contract={contract} />}
             </div>
           ) : (
             <div>
-              <button onClick={() => fuel.connect()}>Connect Wallet</button>
+              <button onClick={() => fuelObj.fuel?.connect()}>
+                Connect Wallet
+              </button>
           </div>
           )}
         </div>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { FuelProvider } from '@fuel-wallet/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <FuelProvider>
     <App />
+    </FuelProvider> 
   </React.StrictMode>
 );
